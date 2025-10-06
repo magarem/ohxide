c0a8eb83aac6ed30b72aa316ac8eb26add7a1bee
marcelo a magalhaes <marceloamagalhaes@marcelos-MacBook-Pro.local>
2025-10-06 15:10:16 -0300

    ip in myproxy file updated

--- server/api/myproxy.ts
+++ server/api/myproxy.ts
@@ -2,7 +2,7 @@ export default defineEventHandler(async (event) => {
     const req = await readBody(event)
     console.log('req:', req);
 
-    let data22 = await $fetch(`http://216.238.98.143:3005/test?year=${req.year}&month=${req.month}&client=${JSON.stringify(req.client).replace(/\s+/g,' ').trim()}&files=${req.files}`);
+    let data22 = await $fetch(`http://77.37.69.142:3005/test?year=${req.year}&month=${req.month}&client=${JSON.stringify(req.client).replace(/\s+/g,' ').trim()}&files=${req.files}`);
     console.log("data22:", data22);
 
     return data22
